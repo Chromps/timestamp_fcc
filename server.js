43700bc36b8f8dd43881f0f6469124bd6d50da4c
Cody Swift <ccswift@me.com>
2017-01-15 15:19:45 -0500

    Working timestamp parsing server

--- server.js
+++ server.js
@@ -2,6 +2,36 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', function(req, res){
-  res.send('Hello World!');
+app.get('/:timestamp', function(req, res){
+  var date;
+  var timestamp = req.params.timestamp;
+    if(+timestamp > 0){
+      date = new Date(+timestamp)
+    }
+    else if(Date.parse(timestamp)){
+      date = new Date(timestamp)
+    }
+    else{
+      res.status(500)
+      res.send({
+        unix: null,
+
+        natural: null
+      })
+      return
+    }
+
+
+    var dateObj = {};
+    dateObj.unix = date.getTime()
+    var nat = `${date.getUTCMonth()} ${date.getDate()}, ${date.getFullYear()}`
+    dateObj.natural = date.toLocaleString('en-us', { month: 'long', day:'numeric', year:'numeric' });
+    res.status(200)
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(dateObj))
+
+
+
+
+
 }).listen(8080)
